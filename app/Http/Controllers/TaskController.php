0f2762f651bab83ce1c57ea6d60ff7caf593ecd2
Exo <paink37@hotmail.com>
2023-07-28 11:01:45 +0100

    Added basic CRUD operations

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\TaskCollection;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Http\Resources\TaskResource;
+use App\Http\Resources\TaskCollection;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\updateTaskRequest;
 
 class TaskController extends Controller
 {
@@ -12,4 +15,30 @@ class TaskController extends Controller
     {
         return new TaskCollection(Task::all());
     }
+
+    public function show(Request $request, Task $task)
+    {
+        return new TaskResource($task);
+    }
+    public function store(StoreTaskRequest $request)
+    {
+        $validated = $request->validated();
+
+        $task = Task::create($validated);
+        return new TaskResource($task);
+    }
+    public function update(updateTaskRequest $request, Task $task)
+    {
+        $validated = $request->validated();
+
+        $task->update($validated);
+        return new TaskResource($task);
+    }
+    public function destroy(updateTaskRequest $request, Task $task)
+    {
+        $task->delete();
+
+        return response()->noContent();
+    }
+
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,6 +9,11 @@ class Task extends Model
 {
     use HasFactory;
 
+
+    protected $fillable = [
+        'title',
+        'is_done'
+    ];
     protected $casts = [
         'is_done' => 'boolean'
     ];
