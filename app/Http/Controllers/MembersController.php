6e7a87a322372b2a248545d60cbd76368119bf55
Exo <paink37@hotmail.com>
2023-08-01 16:03:42 +0100

    Implemented nesting of members in projects

---
+++ app/Http/Controllers/MembersController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+use App\Http\Resources\UserCollection;
+
+class MembersController extends Controller
+{
+    public function index(Request $request, Project $project)
+    {
+        $members = $project->members;
+
+
+        return new UserCollection($members);
+    }
+
+    public function store(Request $request, Project $project)
+    {
+
+        $request->validate([
+            'user_id' => 'required|exists:user,id'
+        ]);
+
+        $project->members()->syncWithoutDetaching([$request->user_id]);
+
+        $members = $project->members;
+
+
+        return new UserCollection($members);
+    }
+
+    public function destroy(Request $request, Project $project, int $member)
+    {
+        abort_if($project->creator_id === $member, 400, 'Cannot renive creator from project.');
+
+        $project->members()->detach([$member]);
+
+        $members = $project->members;
+
+        return new UserCollection($members);
+
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\MembersController;
+use App\Http\Controllers\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,4 +27,5 @@ Route::post('register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('tasks', TaskController::class);
     Route::apiResource('projects', ProjectController::class);
+    Route::apiResource('projects.members', MembersController::class)->only(['index', 'store', 'destroy']);
 });
