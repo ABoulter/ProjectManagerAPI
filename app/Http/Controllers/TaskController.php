b646d1f719f57d57742f2d94491c88c1348c0c6d
Exo <paink37@hotmail.com>
2023-08-02 18:06:28 +0100

    Added scope filters for date ranges

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -23,7 +23,13 @@ class TaskController extends Controller
     public function index(Request $request)
     {
 
-        $tasks = QueryBuilder::for (Task::class)->allowedFilters('is_done')->defaultSort('-created_at')->allowedSorts(['title', 'is_done', 'created_at'])->paginate();
+        $tasks = QueryBuilder::for (Task::class)->allowedFilters([
+            'is_done',
+            'scheduled_at',
+            'due_at',
+            AllowedFilter::scope('scheduled_between'),
+            AllowedFilter::scope('due_between'),
+        ])->defaultSort('-created_at')->allowedSorts(['title', 'is_done', 'created_at'])->paginate();
         return new TaskCollection($tasks);
     }
 
