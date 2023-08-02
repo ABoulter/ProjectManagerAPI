e03adc178e0f37c4d7e5a0dff86244e7fec75728
Exo <paink37@hotmail.com>
2023-08-02 16:50:54 +0100

    Added feature test to store method in Task controller

---
+++ tests/Feature/Controllers/TaskController/TaskControllerStoreTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Feature\TaskController;
+
+use App\Models\Project;
+use Tests\TestCase;
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
+
+/**
+ * ClassNameTest
+ * @group group
+ */
+class TaskControllerStoreTest extends TestCase
+{
+    /** @test */
+    public function test_can_create_task(): void
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $route = route('tasks.store');
+        $response = $this->postJson($route, [
+            'title' => 'foo'
+        ]);
+
+        $response->assertCreated();
+
+        $this->assertDatabaseHas('tasks', [
+            'title' => 'foo',
+            'creator_id' => $user->id,
+        ]);
+    }
+
+    public function test_title_is_required(): void
+    {
+
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $route = route('tasks.store');
+        $response = $this->postJson($route, []);
+
+        $response->assertJsonValidationErrors([
+            'title' => 'required'
+        ]);
+
+    }
+    public function test_cannot_create_tasks_for_unknown_projects(): void
+    {
+
+        $project = Project::factory()->create();
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $route = route('tasks.store');
+        $response = $this->postJson($route, [
+            'title' => 'title',
+            'project_id' => $project->id
+        ]);
+
+        $response->assertJsonValidationErrors([
+            'project_id' => 'in'
+        ]);
+
+    }
+
+
+}
